3051619ed68a4c82f0ec25f6a74ac885a1289cb4
PierrickVouletBonitasoft <pierrick.voulet@bonitasoft.com>
2018-03-22 13:47:26 -0400

    Remove absolute paths in web app sample and use joined resources instead

--- sample-web-app/src/main/java/org/bonitasoft/web/app/EmbeddedBPMListener.java
+++ sample-web-app/src/main/java/org/bonitasoft/web/app/EmbeddedBPMListener.java
@@ -1,28 +1,30 @@
 package org.bonitasoft.web.app;
 
-import java.io.File;
-
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import org.bonitasoft.engine.embedded.bpm.EmbeddedBPM;
 
 public class EmbeddedBPMListener implements ServletContextListener {
-	static private String EMBEDDED_BPM_WEBAPP = System.getProperty("catalina.base") + "/wtpwebapps/sample-web-app/WEB-INF/classes";
-	static private String EMBEDDED_BPM_WORKSPACE = System.getProperty("catalina.base") + "/bonita_embedded_engine";
-	static private String EMBEDDED_BPM_SETUP = EMBEDDED_BPM_WEBAPP + "/setup";
-	static private String EMBEDDED_BPM_EXTRA_CONFIG_FILE = "//embeddedBPM-cfg.xml";
+	
+	static private String EMBEDDED_BPM_SERVER = "embeddedBPMServer";
+	static private String EMBEDDED_BPM_SETUP = "embeddedBPMSetup";
+	static private String EMBEDDED_BPM_CONTEXT = "embeddedBPMContext";
 
-	public void contextInitialized(ServletContextEvent arg0) {
+	public void contextInitialized(ServletContextEvent servletContextEvent) {
+    	System.setProperty(EmbeddedBPM.EMBEDDED_BPM_SERVER_PATH, servletContextEvent.getServletContext().getInitParameter(EMBEDDED_BPM_SERVER));
+    	System.setProperty(EmbeddedBPM.EMBEDDED_BPM_SETUP_PATH, servletContextEvent.getServletContext().getInitParameter(EMBEDDED_BPM_SETUP));
+    	System.setProperty(EmbeddedBPM.EMBEDDED_BPM_CONTEXT_PATH, servletContextEvent.getServletContext().getInitParameter(EMBEDDED_BPM_CONTEXT));
+
 		try {
 			// Start the Bonita BPM Engine
-			EmbeddedBPM.start(new File(EMBEDDED_BPM_WORKSPACE).getAbsolutePath(), new File(EMBEDDED_BPM_SETUP).getAbsolutePath(), new File(EMBEDDED_BPM_EXTRA_CONFIG_FILE).getAbsolutePath());
+			EmbeddedBPM.start();
 		} catch(Exception e) {
 			System.out.println("EmbeddedBPM startup failure: " + e.getMessage());
 		}
 	}
 
-	public void contextDestroyed(ServletContextEvent arg0) {
+	public void contextDestroyed(ServletContextEvent servletContextEvent) {
 		try {
 			// Stop the Bonita BPM Engine
 			EmbeddedBPM.stop();
